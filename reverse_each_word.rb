83b5f2f9fdd7e52596c3d63f3e02da64d8c66ab1
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-22 18:04:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   other = []
-  string.split(" ")
-  string.each do |word|
+  string.split(" ").each do |word|
     other.push(word.reverse)
     end
 "#{other.join(" ")}"
